8cdf35aefae9498fa084e8fb15e782b8dedbac78
SCRAP10063 <weymouth.robotics@gmail.com>
2025-03-07 21:40:49 -0500

    3.7.4

--- src/main/java/frc/robot/commands/LeftReefAuto.java
+++ src/main/java/frc/robot/commands/LeftReefAuto.java
@@ -41,7 +41,7 @@ public class LeftReefAuto extends SequentialCommandGroup {
             // Start at the origin facing the +X direction
             new Pose2d(0, 0, new Rotation2d(0)),
             List.of(new Translation2d(2.82, 0)),
-            new Pose2d(3.66, 0, Rotation2d.fromRadians(Math.PI * 4 / 3)),
+            new Pose2d(2.82, -1.64, Rotation2d.fromRadians(Math.PI * 4 / 3)),
             config);
     PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
     PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
