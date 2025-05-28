8c61f92be13d5317fc9549009dd45425184fddc3
JasrajB29 <jasrajb829@gmail.com>
2025-05-27 22:02:03 -0400

    i see u sean

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -7,6 +7,10 @@ package frc.robot.commands;
 // import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.AlgaeSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
@@ -16,5 +20,15 @@ public final class Autos {
 
   private Autos() {
     throw new UnsupportedOperationException("This is a utility class!");
+  }
+  public static Command coralPlacingAuto(ElevatorSubsystem m_elevatorSubsystem, IntakeSubsystem m_intakeSubsystem, String level) {
+    return Commands.sequence(m_elevatorSubsystem.moveElevatorTo(level),
+    m_intakeSubsystem.runEndEffector(),
+    m_elevatorSubsystem.moveElevatorTo("ZERO"));
+  }
+  public static Command DeAlgaeCommand(ElevatorSubsystem m_elevatorSubsystem, AlgaeSubsystem m_algaeSubsystem, String level) {
+    return Commands.sequence(new RunCommand(m_algaeSubsystem::extendAlgae).withTimeout(2)
+    .andThen(m_elevatorSubsystem.moveElevatorTo(level),
+    Commands.waitSeconds(3).andThen(new RunCommand(m_algaeSubsystem::retractAlgae).withTimeout(1.5))));
   }
 }
